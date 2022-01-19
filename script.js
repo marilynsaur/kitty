dedd12a14841e1cc994915e94f1422738d6247a1
Marilyn Wren <Marilynwren64@yahoo.com>
2022-01-19 13:56:00 -0500

    completed assignment

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "The Proud Kurtles";
+document.querySelector("body").appendChild(h2);
+
+
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('Clickity click!');
+});
+
